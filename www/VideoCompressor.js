f0ca38abf58530b22ff12b0ef0a8285296532df7
AsmitaAK <asmita.koli@gmail.com>
2014-12-04 19:25:55 +0530

    .js updated

--- www/VideoCompressor.js
+++ www/VideoCompressor.js
@@ -1,12 +1,23 @@
- 		cordova.define("com.sanda.videoCompressor.VideoCompressor", 
-				 function(require, exports, module) 
-				 {
-	 				var exec = require('cordova/exec');
+cordova.define("com.sanda.plugins.WatermarkDetector.WatermarkDetector", function(require, exports, module) { var exec = require('cordova/exec');
 
-					 module.exports = {
-							 start: function(successCallback, errorCallback) {
-							        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
-							        exec(successCallback, errorCallback, 'VideoCompressor', 'start', []);
-							    }
-				};
-		});
+module.exports = {
+
+   
+     
+    start: function(successCallback, errorCallback) {
+        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
+        exec(successCallback, errorCallback, 'VideoCompressor', 'start', []);
+    },
+};
+
+});
+
+function success(data)
+{
+alert("success"+data); 
+}
+
+ function failure(data)
+{
+alert("failure");
+}
