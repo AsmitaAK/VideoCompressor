446b97a01250034068f238ff46c1e423b38bd907
AsmitaAK <asmita.koli@gmail.com>
2014-12-04 11:13:57 +0530

    updated plugins.xml

--- www/VideoCompressor.js
+++ www/VideoCompressor.js
@@ -1,47 +1,12 @@
-cordova.define("com.sanda.videoCompressor.VideoCompressor", function(require, exports, module) { var exec = require('cordova/exec');
+ 		cordova.define("com.sanda.videoCompressor.VideoCompressor", 
+				 function(require, exports, module) 
+				 {
+	 				var exec = require('cordova/exec');
 
-/**
- * Provides access to the vibration mechanism on the device.
- */
-
-module.exports = {
-
-    /**
-     * Causes the device to vibrate.
-     *
-     * @param {Integer} mills       The number of milliseconds to vibrate for.
-     */
-     
-    start: function(successCallback, errorCallback) {
-        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
-        exec(successCallback, errorCallback, 'VideoCompressor', 'start', []);
-    },
-};
-
-
-});
- show: function(successCallback, errorCallback) {
-        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
-        exec(successCallback, errorCallback, 'VideoCompressor', 'show', []);
-    },
-};
-
-
-});
-/*
- function successCallback(data){
-	 alert("sanda"+data);
- }
- function errorCallback(data){
-	 alert("error");
- }
-*/
-function success(data)
-{
-alert("success"+data); 
-}
-
- function failure(data)
-{
-alert("failure");
-}
+					 module.exports = {
+							 start: function(successCallback, errorCallback) {
+							        //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
+							        exec(successCallback, errorCallback, 'VideoCompressor', 'start', []);
+							    }
+				};
+		});
